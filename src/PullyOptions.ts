1ee01e099e8130a44d4e376a253e7fd8d20843aa
Kristoffer Venaes Monsen <kris@bitheim.no>
2026-03-13 22:45:59 +0100

    feat: add PULLY_REVIEW_STATUS_ON_NEW_LINE option so that review statuses print in an aligned fashion
    Fixes #39

--- src/PullyOptions.ts
+++ src/PullyOptions.ts
@@ -3,4 +3,5 @@ export interface PullyOptions {
 	PULLY_SLACK_TOKEN: string;
 	PULLY_SLACK_CHANNEL: string;
 	PULLY_HIDE_REPOSITORY_OWNER_IN_SLACK_MESSAGE: boolean;
+	PULLY_REVIEW_STATUS_ON_NEW_LINE: boolean;
 }
